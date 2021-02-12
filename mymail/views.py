a5029c6187e91bf38cbb8663466ca44d57fa77a7
Teejay <tstech3011@gmail.com>
2021-02-12 16:08:04 +0530

    send_mail format updated

--- mymail/views.py
+++ mymail/views.py
@@ -3,12 +3,12 @@ from django.core.mail import send_mail
 
 def index(request):
 
+    mymail_subject = 'Mail from '
     mymail_content = 'I am testing the sending of mail from django. Kya Haal hai Ji'
     mymail_from = 'tejinder3011@hotmail.com'
-    mymail_subject = 'Mail from ' + mymail_from
     mymail_to = ['mefehoj507@combcub.com']
 
-    send_mail(mymail_subject, mymail_content, mymail_from, mymail_to,)
+    send_mail(mymail_subject + mymail_from, mymail_content, mymail_from, mymail_to,)
 
     return render(request, 'mymail/mailsent.html')
 
