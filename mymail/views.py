fe64aebe6da800b7cb4336c8b8490482b14f6221
Teejay <tstech3011@gmail.com>
2021-02-12 16:01:26 +0530

    Sending mail thru gmail working

---
+++ mymail/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def index(request):
+
+    mymail_content = 'I am testing the sending of mail from django. Kya Haal hai Ji'
+    mymail_from = 'tejinder3011@hotmail.com'
+    mymail_subject = 'Mail from ' + mymail_from
+    mymail_to = ['mefehoj507@combcub.com']
+
+    send_mail(mymail_subject, mymail_content, mymail_from, mymail_to,)
+
+    return render(request, 'mymail/mailsent.html')
+
